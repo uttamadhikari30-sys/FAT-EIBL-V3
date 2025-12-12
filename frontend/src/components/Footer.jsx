51c9702c1aa089322e824f781dd43f35b8d6598b
UTTAM ADHIKARI <uttamadhikari30@gmail.com>
2025-12-12 16:04:30 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="auth-footer">
-      © 2025 EDME Services Pvt Ltd.
+      © 2025 Edme Insurance Brokers Limited
       <a href="/privacy-policy">Privacy Policy</a> |
       <a href="/terms">Terms</a> |
       <a href="/contact">Contact Us</a>
